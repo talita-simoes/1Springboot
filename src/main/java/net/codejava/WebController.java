55b02933d8c28ef3e32e9f4051216fb4ee530120
talita.simoes <taiktau@gmail.com>
2021-10-28 10:19:22 +0100

    1Springboot

---
+++ src/main/java/net/codejava/WebController.java
@@ -0,0 +1,14 @@
+package net.codejava;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebController {
+
+    @RequestMapping("/hello")
+    public String sayHello() {
+        System.out.println("Say Hello World Spring boot");
+        return "hello";
+    }
+}
